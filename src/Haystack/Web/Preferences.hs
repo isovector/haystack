9bcda1ff4ce88363af6cc645b4379f043a08159e
Sandy Maguire <sandy@sandymaguire.me>
2015-04-10 22:41:03 -0400

    preference tables

--- src/Haystack/Web/Preferences.hs
+++ src/Haystack/Web/Preferences.hs
@@ -4,7 +4,9 @@ module Haystack.Web.Preferences where
 
 import Haystack.Game
 import Control.Applicative ((<$>))
-import Control.Monad (mapM_)
+import Control.Monad (forM_, mapM_)
+import Data.Char (toLower)
+import Data.String.Utils (replace)
 import Data.Text (Text)
 import Data.Text.Lazy (unpack)
 import Happstack.Lite
@@ -16,37 +18,47 @@ import qualified Text.Blaze.Html5.Attributes as A
 import Haystack.Web.Template
 
 
-prefRow :: String -> H.AttributeValue -> Html
-prefRow s v = H.tr $ do
-                H.td ! A.style "text-align: right" $ label $ H.string (s ++ ":")
-                H.td                               $ mapM_ prefRadio [1..5]
-  where prefRadio :: Int -> Html
-        prefRadio i = input ! type_ "radio" ! name v ! value (H.stringValue $ show i)
 
+prefTable :: Html
+prefTable = H.table $ forM_ [ "Popular"
+                            , "Forgotten Gems"
+                            , "New Releases"
+                            , "Family Games"
+                            , "Party Games"
+                            , "Abstract Strategy Games"
+                            , "2 Player Games"
+                            , "3+ Player Games"
+                            ] makeRow
+  where makeRow s = prefRow s (H.stringValue . map toLower . replace " " "-" $ s)
+        prefRow s v = H.tr $ do
+                        H.td ! A.style "text-align: right" $ label $ H.string (s ++ ":")
+                        H.td                               $ mapM_ prefRadio [1..5]
+          where prefRadio i = input
+                            ! type_ "radio"
+                            ! name v
+                            ! value (H.stringValue $ show i)
 
 
 prefPage :: ServerPart Response
 prefPage = msum [ viewForm, processForm ]
   where
-    viewForm :: ServerPart Response
-    viewForm =
-        do method GET
-           ok $ template "form" $
-              form ! action "/form" ! enctype "multipart/form-data" ! A.method "POST" $ do
-                H.table $ do
-                    prefRow "Popular" "Popular"
-                    prefRow "Forgotten Gems" "ForgottenGem"
-                label ! A.for "msg" $ "Say something clever"
-                input ! type_ "text" ! A.id "msg" ! name "msg"
-                input ! type_ "submit" ! value "Say it!"
+      viewForm :: ServerPart Response
+      viewForm =
+          do method GET
+             ok $ template "form" $
+               form ! action "/form" ! enctype "multipart/form-data" ! A.method "POST" $ do
+                   prefTable
+                   label ! A.for "msg" $ "Say something clever"
+                   input ! type_ "text" ! A.id "msg" ! name "msg"
+                   input ! type_ "submit" ! value "Say it!"
 
-    processForm :: ServerPart Response
-    processForm =
-        do method POST
-           cat :: Category <- read . unpack <$> lookText "category"
-           msg <- lookText "msg"
-           ok $ template "form" $ do
-             H.p "You said:"
-             H.p (toHtml msg)
-             H.p (toHtml $ show cat)
+      processForm :: ServerPart Response
+      processForm =
+          do method POST
+             cat :: Category <- read . unpack <$> lookText "category"
+             msg <- lookText "msg"
+             ok $ template "form" $ do
+                 H.p "You said:"
+                 H.p (toHtml msg)
+                 H.p (toHtml $ show cat)
 
