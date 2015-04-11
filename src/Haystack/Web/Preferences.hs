313998bcf91c556e48d583cea1765d8b7dcc0809
Sandy Maguire <sandy@sandymaguire.me>
2015-04-11 02:44:16 -0400

    preference submission

--- src/Haystack/Web/Preferences.hs
+++ src/Haystack/Web/Preferences.hs
@@ -5,10 +5,7 @@ module Haystack.Web.Preferences where
 import Haystack.Game
 import Control.Applicative ((<$>))
 import Control.Monad (forM_, mapM_)
-import Data.Char (toLower)
-import Data.String.Utils (replace)
 import Data.Text (Text)
-import Data.Text.Lazy (unpack)
 import Happstack.Lite
 import Text.Blaze.Html5 (Html, (!), a, form, input, p, toHtml, label, option, select)
 import Text.Blaze.Html5.Attributes (action, enctype, href, name, size, type_, value)
@@ -17,48 +14,65 @@ import qualified Text.Blaze.Html5.Attributes as A
 
 import Haystack.Web.Template
 
+prefMap :: [(String, String)]
+prefMap = [ ("popular", "Popular")
+          , ("gems", "Forgotten Gems")
+          , ("new", "New Releases")
+          , ("family", "Family Games")
+          , ("party", "Party Games")
+          , ("abstract", "Abstract Games")
+          , ("strategy", "Themed Strategy Games")
+          , ("player2", "2 Player Games")
+          , ("player3", "3+ Player Games")
+          ]
 
+
+noPrefTable :: Html
+noPrefTable = forM_ prefMap noPrefRow
+  where noPrefRow (v, _) = input
+                         ! type_ "hidden"
+                         ! name (H.stringValue v)
+                         ! value "3"
 
 prefTable :: Html
-prefTable = H.table $ forM_ [ "Popular"
-                            , "Forgotten Gems"
-                            , "New Releases"
-                            , "Family Games"
-                            , "Party Games"
-                            , "Abstract Strategy Games"
-                            , "2 Player Games"
-                            , "3+ Player Games"
-                            ] makeRow
-  where makeRow s = prefRow s (H.stringValue . map toLower . replace " " "-" $ s)
-        prefRow s v = H.tr $ do
-                        H.td ! A.style "text-align: right" $ label $ H.string (s ++ ":")
-                        H.td                               $ mapM_ prefRadio [1..5]
-          where prefRadio i = input
-                            ! type_ "radio"
-                            ! name v
-                            ! value (H.stringValue $ show i)
+prefTable = H.table $ forM_ prefMap prefRow
+  where
+      prefRow (v, s) = H.tr $ do
+          H.td ! A.style "text-align: right" $ label $ H.string (s ++ ":")
+          H.td                               $ mapM_ prefRadio [1..5]
+        where prefRadio i = input
+                          ! type_ "radio"
+                          ! name (H.stringValue v)
+                          ! value (H.stringValue $ show i)
+
+runForm :: H.AttributeValue -> Html -> Html
+runForm submit contents =
+    form ! action "/form"
+         ! enctype "multipart/form-data"
+         ! A.method "POST" $ do
+             contents
+             input ! type_ "submit" ! value submit
+
 
 
 prefPage :: ServerPart Response
 prefPage = msum [ viewForm, processForm ]
   where
-      viewForm :: ServerPart Response
       viewForm =
           do method GET
-             ok $ template "form" $
-               form ! action "/form" ! enctype "multipart/form-data" ! A.method "POST" $ do
-                   prefTable
-                   label ! A.for "msg" $ "Say something clever"
-                   input ! type_ "text" ! A.id "msg" ! name "msg"
-                   input ! type_ "submit" ! value "Say it!"
+             ok $ template "form" $ do
+               runForm "No Preferences" noPrefTable
+               runForm "Submit!" prefTable
 
-      processForm :: ServerPart Response
       processForm =
           do method POST
-             cat :: Category <- read . unpack <$> lookText "category"
-             msg <- lookText "msg"
+             prefs <- sequence $ map (runText . fst) prefMap
              ok $ template "form" $ do
                  H.p "You said:"
-                 H.p (toHtml msg)
-                 H.p (toHtml $ show cat)
+                 forM_ prefs output
 
+      runText idx = do rating <- lookText idx
+                       return (idx, rating)
+
+      output (idx, val) = do H.p $ toHtml idx
+                             H.p $ toHtml $ show val
